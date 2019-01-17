037512b20724ae75223d4d8f62b6547ae374b407
JuanmaPT <46747317+JuanmaPT@users.noreply.github.com>
2019-01-18 00:21:13 +0100

    Go part4 completed and checked

--- Part4/go/foo.go
+++ Part4/go/foo.go
@@ -3,7 +3,7 @@
 package main
 
 import (
-    . "fmt"
+    "fmt"
     "runtime"
     "time"
 )
@@ -12,20 +12,32 @@ var i = 0
 
 func incrementing() {
     //TODO: increment i 1000000 times
+    for a := 0; a < 1000000; i++ {
+        i = i + 1
+    }
 }
 
 func decrementing() {
     //TODO: decrement i 1000000 times
+    for a := 0; a < 1000000; i++ {
+        i = i + 1
+    }
+}
+
+func getGOMAXPROCS() int {
+    return runtime.GOMAXPROCS(0)
 }
 
 func main() {
+    //fmt.Printf("GOMAXPROCS before assigning nCPUs is %d\n", getGOMAXPROCS())
     runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
 	                                    // Try doing the exercise both with and without it!
-
+    //fmt.Printf("GOMAXPROCS after assigning nCPUs is %d\n", getGOMAXPROCS())
     // TODO: Spawn both functions as goroutines
-	
+	go incrementing()
+    go decrementing()
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
     // We'll come back to using channels in Exercise 2. For now: Sleep.
     time.Sleep(100*time.Millisecond)
-    Println("The magic number is:", i)
+    fmt.println("The magic number is:", i)
 }
